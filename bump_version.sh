da12b4858f78cf6da510caa8358019e0e04e85e1
Alexander Terp <alexander.terp@gmail.com>
2024-09-08 20:06:15 +1000

    Add bump_version.sh
    A little script to bump rad's version, performing the following tasks:
    
    0. parse args
    1. update version in cobra_root.go with sed
    2. make a commit updating that file
    3. tag that commit
    4. optionally push
    
    for step 0, we dogfood rad's --BASH functionality a bit, by letting *it*
    handle the argument parsing.

---
+++ bump_version.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+rsl="\
+\"\"\"
+Bumps the version in rad, creates a commit, tags it, and optionally pushes it
+\"\"\"
+args:
+    new_version string # The new version to bump to
+    push p bool # Whether or not a push should also be performed"
+
+eval "$(rad --STDIN --BASH $* <<< "$rsl")" || exit 1
+
+# Update Version in ./core/cobra_root.go
+sed -i '' "s/Version: \".*\"/Version: \"$new_version\"/" ./core/cobra_root.go || exit 1
+git add ./core/cobra_root.go
+git commit -m "Bump version to $new_version"
+git tag -a "$new_version" -m "Bump version to $new_version" || exit 1
+
+if [ "$push" = true ]; then
+    echo "Pushing..."
+    git push origin main --tags || exit 1
+else
+    echo "Tagged, not pushing..."
+    echo "Run 'git push origin main --tags' to push the tag"
+fi
