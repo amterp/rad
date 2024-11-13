78dc9fdd18f17294496ddf87508ea132e0e21349
Alexander Terp <alexander.terp@gmail.com>
2024-11-13 22:03:38 +1100

    Bump version to 0.4.10

--- core/cobra_root.go
+++ core/cobra_root.go
@@ -22,7 +22,7 @@ func NewRootCmd(cmdInput CmdInput) *cobra.Command {
 		Use:     "",
 		Short:   "Request And Display (RAD)",
 		Long:    `Request And Display (RAD): A tool for making HTTP requests, extracting details, and displaying the result.`,
-		Version: "0.4.9",
+		Version: "0.4.10",
 		FParseErrWhitelist: cobra.FParseErrWhitelist{
 			UnknownFlags: true,
 		},
