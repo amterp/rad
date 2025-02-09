296e5ddbe60f6783656a58fd8bc2f896612b881f
Alexander Terp <alexander.terp@gmail.com>
2025-02-09 21:44:36 +0000

    Revamp: Implement function 'truncate'

--- core/rsl_functions.go
+++ core/rsl_functions.go
@@ -50,10 +50,6 @@ func runRslNonVoidFunction(i *MainInterpreter, function Token, numExpectedReturn
 	//	default:
 	//		i.error(function, fmt.Sprintf("%s() takes a map, got %s", VALUES, TypeAsString(args[0])))
 	//	}
-	//case TRUNCATE:
-	//	assertExpectedNumReturnValuesOld(i, function, funcName, numExpectedReturnValues, ONE_ARG)
-	//	validateExpectedNamedArgsOld(i, function, NO_NAMED_ARGS, namedArgsMap)
-	//	return runTruncate(i, function, args)
 	//case SPLIT:
 	//	assertExpectedNumReturnValuesOld(i, function, funcName, numExpectedReturnValues, ONE_ARG)
 	//	validateExpectedNamedArgsOld(i, function, NO_NAMED_ARGS, namedArgsMap)
