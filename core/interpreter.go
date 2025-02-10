0c39c525e9fd4c49faaa9356810aace4d83d5194
Alexander Terp <alexander.terp@gmail.com>
2025-02-10 12:40:53 +0000

    Revamp: Rename 'new' files
    I had prefixed these files with 'new_' to differentiate them from the
    'old' implemenation files. Those old ones are now deleted, so we can
    rename these.

--- core/interpreter.go
+++ core/interpreter.go
@@ -29,6 +29,33 @@ func NewInterpreter(scriptData *ScriptData) *Interpreter {
 	return i
 }
 
+func (i *Interpreter) InitArgs(args []RslArg) {
+	env := i.env
+
+	for _, arg := range args {
+		switch coerced := arg.(type) {
+		case *BoolRslArg:
+			env.SetVar(coerced.Identifier, newRslValue(i, arg.GetNode(), coerced.Value))
+		case *BoolArrRslArg:
+			env.SetVar(coerced.Identifier, newRslValue(i, arg.GetNode(), NewRslListFromGeneric(i, arg.GetNode(), coerced.Value)))
+		case *StringRslArg:
+			env.SetVar(coerced.Identifier, newRslValue(i, arg.GetNode(), coerced.Value))
+		case *StringArrRslArg:
+			env.SetVar(coerced.Identifier, newRslValue(i, arg.GetNode(), NewRslListFromGeneric(i, arg.GetNode(), coerced.Value)))
+		case *IntRslArg:
+			env.SetVar(coerced.Identifier, newRslValue(i, arg.GetNode(), coerced.Value))
+		case *IntArrRslArg:
+			env.SetVar(coerced.Identifier, newRslValue(i, arg.GetNode(), NewRslListFromGeneric(i, arg.GetNode(), coerced.Value)))
+		case *FloatRslArg:
+			env.SetVar(coerced.Identifier, newRslValue(i, arg.GetNode(), coerced.Value))
+		case *FloatArrRslArg:
+			env.SetVar(coerced.Identifier, newRslValue(i, arg.GetNode(), NewRslListFromGeneric(i, arg.GetNode(), coerced.Value)))
+		default:
+			i.errorf(arg.GetNode(), "Unsupported arg type, cannot init: %T", arg)
+		}
+	}
+}
+
 func (i *Interpreter) Run() {
 	node := i.sd.Tree.Root()
 	i.recursivelyRun(node)
